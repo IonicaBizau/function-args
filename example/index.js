7c45d036756b9641282ae581513bd2f2f0fb96d9
Ionică Bizău <bizauionica@gmail.com>
2016-02-23 20:11:45 +0200

    :rocket:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,28 @@
 
 const functionArgs = require("../lib");
 
-console.log(functionArgs());
+console.log(functionArgs(x => x * x));
+// [ 'x' ]
+
+console.log(functionArgs((a, b) => a + b));
+// [ 'a', ' b' ]
+
+console.log(functionArgs(function (a, b) { return a + b; }));
+// [ 'a', ' b' ]
+
+class Foo {
+    static bar (a, b) {}
+    baz (a, b) {}
+}
+
+console.log(functionArgs(Foo.bar));
+// [ 'a', ' b' ]
+
+console.log(functionArgs((new Foo()).baz));
+// [ 'a', ' b' ]
+
+console.log(functionArgs(function () {}));
+// []
+
+console.log(functionArgs(() => {}));
+// []
